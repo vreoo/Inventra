ad09b81fe72577db47180c76747ab1442d708953
vreoo <nibrasshami2002@gmail.com>
2025-10-13 14:02:42 +0300

    feat: update CORS settings and refactor Loading component
    - Added a new frontend URL to the CORS middleware in the FastAPI backend for deployment.
    - Refactored the Loading component in the frontend to improve code readability and maintainability, including consistent indentation and formatting.

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Frontend URLs
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://inventra-plum.vercel.app/"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
